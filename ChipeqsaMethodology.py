472140252b1c6e6eb05725efe127239bff02d561
Vertigo2810 <93049946+Vertigo2810@users.noreply.github.com>
2022-06-02 20:05:05 +0400

    Update ChipeqsaMethodology.py

--- ChipeqsaMethodology.py
+++ ChipeqsaMethodology.py
@@ -44,7 +44,7 @@ money = int(input('''\033[0;37m
 [Pasuxi : (1-3) ]> '''))
 
 if money == 1:
-    text = "\033[0;91m[+] Karoche shedixar ormocda merve yazaxetis ligashi da irchev \n    mesame tamashis 2 kushs, uechveli meitans \n <3    "
+    text = "\033[0;91m[+] Karoche shedixar ormocda merve yazaxetis ligashi da irchev \n    mesame tamashis 2 kushs, uechveli meitans \n "
     for x in text:
       sleep(0.02) # In seconds
       sys.stdout.write(x)
@@ -52,7 +52,7 @@ if money == 1:
 
 
 elif money == 2:
-        text = "\033[0;91m[+] Mai aritamasho dzma \n    Magan damaqcia ro movutyan momgoni \n <3    "
+        text = "\033[0;91m[+] Mai aritamasho dzma \n    Magan damaqcia ro movutyan momgoni \n "
         for x in text:
           sleep(0.02) # In seconds
           sys.stdout.write(x)
@@ -61,7 +61,7 @@ elif money == 2:
 
 
 elif money == 3:
-        text = "\033[0;91m[+] Patara mayutit magali X \n   An didi mayutit 1.20 X \n <3    "
+        text = "\033[0;91m[+] Patara mayutit magali X \n   An didi mayutit 1.20 X \n "
         for x in text:
           sleep(0.02) # In seconds
           sys.stdout.write(x)
